5f8c89da0318090fa66f65539805ea855a5ad3f4
Wendy Dherin <wendydherin@gmail.com>
2020-11-12 18:22:23 +0100

    Get landing page set in all breakpoints.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-scroll';
 
 const NavBar = (props) => {
   return props.navItems.map((navItem) => (
-    <Fade top duration={800} delay={4000} distance="30px">
+    <Fade key={navItem.text} top duration={800} delay={4000} distance="30px">
       <Link to={navItem.to} smooth duration={1000}>
         <h2 className="nav-item skew">{navItem.text}</h2>
       </Link>
