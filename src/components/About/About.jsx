5e44d03b4d7def8d2bb8de9ed5ba0f3d5db98235
Wendy Dherin <wendydherin@gmail.com>
2020-11-12 12:10:20 +0100

    Add social link icons to About page and give white prompts a lightly dotted border.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Row, Col } from 'react-bootstrap';
+
+import Prompt from '../common/Prompt';
 import FixedImage from '../Image/FixedImage';
 import ContentContext from '../../contexts/content';
 
@@ -37,18 +39,40 @@ const About = () => {
       <Row>
         <Col md={6} sm={12} className="left">
           <Fade left duration={1000} delay={600} distance="30px">
-            <FixedImage alt={alt} filename={img} />
+            <FixedImage
+              imgStyle={{ objectFit: 'contain' }}
+              style={{ height: '350px' }}
+              alt={alt}
+              filename={img}
+            />
           </Fade>
-          <Fade left duration={1000} delay={1000} distance="30px">
-            <h3>{name}</h3>
-          </Fade>
-          <Fade right duration={1000} delay={1000} distance="30px">
-            <p>{accreditation}</p>
-          </Fade>
-          <Fade bottom duration={1000} delay={1000} distance="30px">
-            <p className="social">{linkedIn}</p>
-            <p className="social">{xing}</p>
-          </Fade>
+          <div className="details">
+            <Fade left duration={1000} delay={1000} distance="30px">
+              <h3>{name}</h3>
+            </Fade>
+            <Fade right duration={1000} delay={1000} distance="30px">
+              <p>{accreditation}</p>
+            </Fade>
+            <Fade bottom duration={1000} delay={1000} distance="30px">
+              <p className="social">
+                <a href="https://www.linkedin.com/in/thelan/" target="_blank" rel="noreferrer">
+                  <span>
+                    <i className="fa fa-linkedin-square" style={{ fontSize: '24px' }} />{' '}
+                  </span>
+                  {linkedIn}
+                </a>
+              </p>
+              <p className="social">
+                <a href="https://xing.com/profile/thelan_nguyen" target="_blank" rel="noreferrer">
+                  <span>
+                    <i className="fa fa-xing" style={{ fontSize: '24px' }} />{' '}
+                  </span>
+                  {xing}
+                </a>
+              </p>
+            </Fade>
+          </div>
+          <Prompt linkTo="contact" text="GET IN TOUCH" />
         </Col>
         <Col md={6} sm={12} className="right">
           <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
--- src/html.js
+++ src/html.js
@@ -20,6 +20,10 @@ export default function HTML({
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         {headComponents}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
       </head>
       <body {...bodyAttributes}>
         {preBodyComponents}
